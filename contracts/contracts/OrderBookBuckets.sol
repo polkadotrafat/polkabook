1806b559453a75aed634fe0aeef58b652d3a7d7a
Rafat Hussain <rafat.hsn@gmail.com>
2026-03-13 13:46:57 +0530

    linkedlist impelemented in orderbookbuckets

--- contracts/contracts/OrderBookBuckets.sol
+++ contracts/contracts/OrderBookBuckets.sol
@@ -39,6 +39,8 @@ contract OrderBookBuckets is Ownable {
         uint64[] orderIds;
         uint64 headIndex;
         uint128 totalOpenQuantity;
+        uint128 nextPrice;
+        uint128 prevPrice;
     }
 
     struct QuoteResult {
@@ -71,8 +73,8 @@ contract OrderBookBuckets is Ownable {
 
     mapping(uint64 orderId => OrderRecord order) public orders;
     mapping(uint8 side => mapping(uint128 price => PriceLevel level)) internal priceLevels;
-    uint128[] internal bidPrices;
-    uint128[] internal askPrices;
+    uint128 public bestBid;
+    uint128 public bestAsk;
 
     event OrderPlaced(
         uint64 indexed orderId,
@@ -148,6 +150,9 @@ contract OrderBookBuckets is Ownable {
         if (openQuantity > 0) {
             PriceLevel storage level = priceLevels[order.side][order.price];
             level.totalOpenQuantity -= openQuantity;
+            if (level.totalOpenQuantity == 0) {
+                _removePriceLevel(order.side, order.price);
+            }
             if (level.headIndex < level.orderIds.length && level.orderIds[level.headIndex] == orderId) {
                 _advanceHeadIfNeeded(order.side, order.price);
             }
@@ -242,10 +247,18 @@ contract OrderBookBuckets is Ownable {
     }
 
     function getPriceLevels(uint8 side) external view returns (uint128[] memory levels) {
-        uint128[] storage source = side == SIDE_BID ? bidPrices : askPrices;
-        levels = new uint128[](source.length);
-        for (uint256 i = 0; i < source.length; i++) {
-            levels[i] = source[i];
+        uint256 count = 0;
+        uint128 current = side == SIDE_BID ? bestBid : bestAsk;
+        while (current != 0) {
+            count++;
+            current = priceLevels[side][current].nextPrice;
+        }
+
+        levels = new uint128[](count);
+        current = side == SIDE_BID ? bestBid : bestAsk;
+        for (uint256 i = 0; i < count; i++) {
+            levels[i] = current;
+            current = priceLevels[side][current].nextPrice;
         }
     }
 
@@ -322,7 +335,14 @@ contract OrderBookBuckets is Ownable {
         askOrder.reservedAmount -= trade.quantity;
 
         priceLevels[SIDE_BID][bidOrder.price].totalOpenQuantity -= trade.quantity;
+        if (priceLevels[SIDE_BID][bidOrder.price].totalOpenQuantity == 0) {
+            _removePriceLevel(SIDE_BID, bidOrder.price);
+        }
+
         priceLevels[SIDE_ASK][askOrder.price].totalOpenQuantity -= trade.quantity;
+        if (priceLevels[SIDE_ASK][askOrder.price].totalOpenQuantity == 0) {
+            _removePriceLevel(SIDE_ASK, askOrder.price);
+        }
 
         vault.settleTrade(
             bidOrder.trader,
@@ -348,17 +368,14 @@ contract OrderBookBuckets is Ownable {
         uint8 side,
         uint256 depth
     ) internal view returns (MatcherCodec.Order[] memory topOrders) {
-        uint128[] storage levels = side == SIDE_BID ? bidPrices : askPrices;
         MatcherCodec.Order[] memory scratch = new MatcherCodec.Order[](depth);
         uint256 count;
-        uint256 levelCount = levels.length;
+        uint128 currentPrice = side == SIDE_BID ? bestBid : bestAsk;
 
-        for (uint256 i = 0; i < levelCount && count < depth; ) {
-            PriceLevel storage level = priceLevels[side][levels[i]];
+        while (currentPrice != 0 && count < depth) {
+            PriceLevel storage level = priceLevels[side][currentPrice];
             if (level.totalOpenQuantity == 0) {
-                unchecked {
-                    ++i;
-                }
+                currentPrice = level.nextPrice;
                 continue;
             }
 
@@ -386,9 +403,7 @@ contract OrderBookBuckets is Ownable {
                 }
             }
 
-            unchecked {
-                ++i;
-            }
+            currentPrice = level.nextPrice;
         }
 
         topOrders = new MatcherCodec.Order[](count);
@@ -402,7 +417,7 @@ contract OrderBookBuckets is Ownable {
 
     function _appendToPriceLevel(uint64 orderId, uint8 side, uint128 price, uint128 quantity) internal {
         PriceLevel storage level = priceLevels[side][price];
-        if (level.orderIds.length == 0) {
+        if (level.totalOpenQuantity == 0) {
             _insertPriceLevel(side, price);
         }
 
@@ -411,22 +426,61 @@ contract OrderBookBuckets is Ownable {
     }
 
     function _insertPriceLevel(uint8 side, uint128 price) internal {
-        uint128[] storage levels = side == SIDE_BID ? bidPrices : askPrices;
-        levels.push(price);
+        uint128 current = side == SIDE_BID ? bestBid : bestAsk;
+        
+        if (current == 0) {
+            if (side == SIDE_BID) bestBid = price;
+            else bestAsk = price;
+            return;
+        }
 
-        uint256 index = levels.length - 1;
-        while (index > 0) {
-            uint128 previous = levels[index - 1];
-            bool inOrder = side == SIDE_BID ? previous > price : previous < price;
-            if (inOrder) {
+        uint128 prev = 0;
+        while (current != 0) {
+            bool inOrder = side == SIDE_BID ? current > price : current < price;
+            if (!inOrder) {
                 break;
             }
-            levels[index] = previous;
-            unchecked {
-                --index;
-            }
+            prev = current;
+            current = priceLevels[side][current].nextPrice;
         }
-        levels[index] = price;
+
+        PriceLevel storage newLevel = priceLevels[side][price];
+        newLevel.prevPrice = prev;
+        newLevel.nextPrice = current;
+
+        if (prev == 0) {
+            if (side == SIDE_BID) bestBid = price;
+            else bestAsk = price;
+        } else {
+            priceLevels[side][prev].nextPrice = price;
+        }
+
+        if (current != 0) {
+            priceLevels[side][current].prevPrice = price;
+        }
+    }
+
+    function _removePriceLevel(uint8 side, uint128 price) internal {
+        PriceLevel storage level = priceLevels[side][price];
+        uint128 prev = level.prevPrice;
+        uint128 next = level.nextPrice;
+
+        if (prev == 0) {
+            if (side == SIDE_BID) {
+                if (bestBid == price) bestBid = next;
+            } else {
+                if (bestAsk == price) bestAsk = next;
+            }
+        } else {
+            priceLevels[side][prev].nextPrice = next;
+        }
+
+        if (next != 0) {
+            priceLevels[side][next].prevPrice = prev;
+        }
+
+        level.prevPrice = 0;
+        level.nextPrice = 0;
     }
 
     function _advanceHeadIfNeeded(uint8 side, uint128 price) internal {
@@ -516,18 +570,15 @@ contract OrderBookBuckets is Ownable {
         MatcherCodec.Order memory hypotheticalOrder,
         bool isBidSide
     ) internal view returns (MatcherCodec.Order[] memory topOrders) {
-        uint128[] storage levels = side == SIDE_BID ? bidPrices : askPrices;
         MatcherCodec.Order[] memory scratch = new MatcherCodec.Order[](depth);
         uint256 count;
-        uint256 levelCount = levels.length;
+        uint128 currentPrice = side == SIDE_BID ? bestBid : bestAsk;
         bool inserted;
 
-        for (uint256 i = 0; i < levelCount && count < depth; ) {
-            PriceLevel storage level = priceLevels[side][levels[i]];
+        while (currentPrice != 0 && count < depth) {
+            PriceLevel storage level = priceLevels[side][currentPrice];
             if (level.totalOpenQuantity == 0) {
-                unchecked {
-                    ++i;
-                }
+                currentPrice = level.nextPrice;
                 continue;
             }
 
@@ -568,9 +619,7 @@ contract OrderBookBuckets is Ownable {
                 }
             }
 
-            unchecked {
-                ++i;
-            }
+            currentPrice = level.nextPrice;
         }
 
         if (!inserted && count < depth) {
@@ -614,18 +663,9 @@ contract OrderBookBuckets is Ownable {
     }
 
     function _bestActiveLevel(uint8 side) internal view returns (uint128 price, uint128 quantity) {
-        uint128[] storage levels = side == SIDE_BID ? bidPrices : askPrices;
-        uint256 levelCount = levels.length;
-
-        for (uint256 i = 0; i < levelCount; ) {
-            uint128 levelPrice = levels[i];
-            PriceLevel storage level = priceLevels[side][levelPrice];
-            if (level.totalOpenQuantity != 0) {
-                return (levelPrice, level.totalOpenQuantity);
-            }
-            unchecked {
-                ++i;
-            }
+        price = side == SIDE_BID ? bestBid : bestAsk;
+        if (price != 0) {
+            quantity = priceLevels[side][price].totalOpenQuantity;
         }
     }
 }
